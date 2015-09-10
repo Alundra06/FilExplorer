db25ee36e037df6ece33a2046c35c79b56a0f396
Ouassim Sadellah <ouassim.sadellah@gmail.com>
2015-09-10 17:16:27 -0400

    First Commit
    Created the solution in VS

---
+++ FilExplorer/FilExplorer/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FilExplorer.MvcApplication" Language="C#" %>
---
+++ FilExplorer/FilExplorer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FilExplorer.Startup))]
+namespace FilExplorer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
