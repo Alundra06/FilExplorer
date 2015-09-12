3d3b02d8eb12eba673b8db99a293dc87e683a90a
Ouassim Sadellah <ouassim.sadellah@gmail.com>
2015-09-12 18:46:46 -0400

    trying to fix the email Data model issue

--- FilExplorer/FilExplorer.DataLayer/Models/AccountViewModels.cs
+++ FilExplorer/FilExplorer.DataLayer/Models/AccountViewModels.cs
@@ -10,42 +10,6 @@ namespace FilExplorer.DataLayer.Models
         public string UserName { get; set; }
     }
 
-    //public class ExternalLoginListViewModel
-    //{
-    //    public string ReturnUrl { get; set; }
-    //}
-
-    //public class SendCodeViewModel
-    //{
-    //    public string SelectedProvider { get; set; }
-    //    public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-    //    public string ReturnUrl { get; set; }
-    //    public bool RememberMe { get; set; }
-    //}
-
-    //public class VerifyCodeViewModel
-    //{
-    //    [Required]
-    //    public string Provider { get; set; }
-
-    //    [Required]
-    //    [Display(Name = "Code")]
-    //    public string Code { get; set; }
-    //    public string ReturnUrl { get; set; }
-
-    //    [Display(Name = "Remember this browser?")]
-    //    public bool RememberBrowser { get; set; }
-
-    //    public bool RememberMe { get; set; }
-    //}
-
-    //public class ForgotViewModel
-    //{
-    //    [Required]
-    //    [Display(Name = "Email")]
-    //    public string Email { get; set; }
-    //}
-
     public class LoginViewModel
     {
         [Required]
@@ -65,8 +29,8 @@ namespace FilExplorer.DataLayer.Models
     {
         [Required]
         [EmailAddress]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
 
         [Required]
         [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
